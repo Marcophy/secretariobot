d12fdd1dc7e83d9d33121a950f67bb4824a6e59b
Marco A. Villena <mavillenas@hotmail.com>
2023-09-27 21:00:17 +0300

    Fist commit. Main file added with part of the code.

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+from dotenv import load_dotenv
+import ssl
+from email.message import EmailMessage
+import smtplib
+import imaplib
+import email
+import re
+
+
+def check_email(in_email_address, in_password, in_folder='inbox'):
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login(in_email_address, in_password)
+    mail.list()  # Out: list of "folders" aka labels in gmail.
+    mail.select(in_folder)  # connect to inbox.
+
+    result, data = mail.search(None, "ALL")
+
+    ids = data[0]  # los datos son una lista.
+    id_list = ids.split()  # IDS es una cadena separada por espacios
+    latest_email_id = id_list[-1]  # Obtén el ultimo
+
+    result, data = mail.fetch(latest_email_id, "(RFC822)")  # buscar el cuerpo del correo electrónico (RFC822) para la identificación dada
+
+    raw_email = data[0][1]  # here's the body, which is raw text of the whole email # including headers and alternate payloads
+
+    email_message = email.message_from_string(raw_email.decode('utf-8'))
+
+    # Get the email address clean
+    pattern = r'<(.*?)>'
+    sender_email = re.findall(pattern, email_message['From'])
+
+    return email_message['Subject'], email_message['Date'], sender_email
+
+
+def send_email(in_password, in_from, in_to, in_subject, in_body):
+    em = EmailMessage()  # Initilize this variable
+    em['From'] = in_from
+    em['To'] = in_to
+    em['Subject'] = in_subject
+    em.set_content(in_body)
+
+    # Initialize the email
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(in_from, in_password)
+            smtp.sendmail(in_from, in_to, em.as_string())
+            smtp.quit()
+            return True
+
+    except Exception as err:
+        print("ERROR:", err)
+        return False
+
+
+# ------- MAIN -------
+
+# Get the password
+load_dotenv()
+
+email_address = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+key_subject = 'myip'
+
+while True:
+    pass
