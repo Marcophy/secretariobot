a0ac28c0cc3accac8d654082e839cc96b88a858f
Marco A. Villena <43510394+Marcophy@users.noreply.github.com>
2024-06-07 19:37:22 +0200

    Update main.py
    Email update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Marco A. Villena, PhD.
 # ****** dunder variables ******
 __project_name__ = "SECRETARIOBOT"
 __author__ = "Marco A. Villena"
-__email__ = "mavillenas@proton.me"
+__email__ = "mavillena@ugr.es"
 __version__ = "0.2"
 __project_date__ = '2023'
 
